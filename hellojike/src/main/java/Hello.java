017f14e9365333d3c97c6e6ad26cfa51e16baf97
490823567@qq.com <490823567@qq.com>
2021-06-26 20:30:39 +0800

    jike

---
+++ hellojike/src/main/java/Hello.java
@@ -0,0 +1,20 @@
+/**
+ * @author tangmi
+ * @title: Hello
+ * @projectName hellojike
+ * @description: TODO
+ * @date 2021/6/26 18:49
+ */
+public class Hello {
+    public static void main(String[] args) {
+        int i = 0;
+        int j = 1;
+        int k;
+        k = 66;
+        int all = 0;
+        for (int m = 0; m < k; m++) {
+            all = all + m;
+        }
+        System.out.println(all);
+    }
+}
